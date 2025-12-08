415a5edea454a83a4b2467b9afdf5b9351977569
Maxbaka17 <maxmorals1717@gmail.com>
2025-12-08 21:32:05 +0530

    sprits and picking up mechanics(not done)

---
+++ characters/her/her.gd
@@ -0,0 +1,90 @@
+extends CharacterBody2D
+
+var speed = 50
+var he_is_close = false
+var picked_up = false
+@onready var him : CharacterBody2D
+@onready var sprite_2d = $Sprite2D
+@onready var animation_player = $AnimationPlayer
+@onready var arm = $Sprite2D/arm
+@onready var camera = $Camera2D
+
+func _physics_process(delta):
+	him = get_tree().get_first_node_in_group("him")
+	var direction = Input.get_axis("left","right")
+	
+	velocity.x = speed * direction
+	sprite_slip(direction)
+	animation()
+	gravity()
+	if picked_up:
+		visible = false
+		global_position = Vector2(0,0)
+		
+	else:
+		visible = true
+		if Input.is_action_just_pressed("pick_up"):
+			pick_up()
+	
+	
+		
+	move_and_slide()
+	
+	
+func gravity():
+	if not picked_up:
+		if not is_on_floor():
+			velocity.y += 600
+			
+		else:
+			velocity.y = 0
+func animation():
+	
+	if abs(velocity.x) > 0:
+		animation_player.play("side walk her")
+		
+	else:
+		animation_player.play("front_idle her")
+		
+	
+			
+func sprite_slip(dir):
+	if dir == 1:
+		sprite_2d.flip_h = true
+	elif dir == -1:
+		sprite_2d.flip_h = false
+
+
+func pick_up():
+	
+	if he_is_close:
+		if not picked_up:
+			#velocity.x = move_toward(velocity.x,him.global_position.x,speed)
+			him.camera.enabled = true
+			camera.enabled = false
+			picked_up = true
+			him.picked_up = true
+			print("pick_up")
+		else:
+			picked_up = false
+			him.picked_up = false
+			
+func hold_hand():
+	pass
+
+func _on_area_2d_area_entered(area):
+	
+	var area_parent = area.get_parent()
+	if area_parent.is_in_group("him"):
+		area_parent.following_her = false
+		he_is_close = true
+	
+
+func _on_area_2d_area_exited(area):
+	var area_parent = area.get_parent()
+	if area_parent.is_in_group("him"):
+		he_is_close = false
+		#
+		pass
+		#area_parent.following_her = true
+		
---
+++ characters/him/him.gd
@@ -0,0 +1,102 @@
+extends CharacterBody2D
+
+
+var speeed = 60
+var holding_hands := false
+var picked_up := false
+var following_her = true
+var behind_her = false
+var in_control = false
+var her_position
+@onready var her : CharacterBody2D
+
+@onready var sprite_2d = $Sprite2D
+@onready var animation_player = $AnimationPlayer
+@onready var camera = $Camera2D
+
+func _physics_process(delta):
+	her = get_tree().get_first_node_in_group("Queen")
+	var direction = Input.get_axis("left","right")
+	
+	if Input.is_action_just_pressed("follow"):
+		if not her.he_is_close:
+			following_her = true
+			
+		else:
+			following_her = false
+		
+		
+	if not in_control:
+		follow_her(delta)
+		
+	if picked_up:
+		in_control = true
+		
+	else:
+		in_control = false
+		
+	if her.picked_up:
+		if her.global_position == Vector2(0,0):
+			if not her.he_is_close:
+				if Input.is_action_just_pressed("pick_up"):
+					her.picked_up = false
+					picked_up = false
+					her.camera.enabled = true
+					camera.enabled = false
+					her.global_position.x = global_position.x - 20
+	#print(velocity.x)
+	animation()
+	move(direction)
+	sprite_slip(direction)
+	gravity()
+	move_and_slide()
+	pass
+	
+func gravity():
+	if not is_on_floor():
+		velocity.y += 600
+	
+	else:
+		velocity.y = 0
+		
+func sprite_slip(dir):
+	if not in_control:
+		if velocity.x > 0:
+			sprite_2d.flip_h = true
+		else:
+			sprite_2d.flip_h = false
+			
+	else:
+		if dir == 1:
+			sprite_2d.flip_h = true
+		elif dir == -1:
+			sprite_2d.flip_h = false
+			
+func move(dir):
+	if in_control:
+		velocity.x = 300 * dir
+	
+func animation():
+	if not picked_up:
+		if abs(velocity.x) > 0:
+			animation_player.play("side running me")
+			
+		else:
+			animation_player.play("front idle me")
+		
+	else:
+		if abs(velocity.x) > 0:
+			animation_player.play("running holding her")
+			
+		else:
+			animation_player.play("holding her idle")
+			
+		
+func follow_her(delta):
+	if following_her:
+		if global_position != her.global_position:
+			
+			velocity.x = move_toward(velocity.x,her.global_position.x,speeed * delta)
+			
+	else:
+		velocity.x = move_toward(velocity.x,0,speeed)
